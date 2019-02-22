517e949ef351f278cb62100ebddbe5913f667eb0
Phil Steinhorst <phist91@gmail.com>
2019-02-22 20:42:14 +0100

    finished gui implementation

---
+++ code/MarkSweep-timed.java
@@ -0,0 +1,20 @@
+	private void markStartTimed() {
+		toScan.clear();
+		// zunächst Iteration über roots
+		Iterator<HeapObject> iterator = controller.getRoots().iterator();      
+		gcTimer = new Timer(Settings.ANIMATION_SPEED, action -> {
+			// mit jeder Timer-Iteration ein Basisobjekt abarbeiten
+			if (iterator.hasNext()) {
+				HeapObject obj = iterator.next(); 
+				if (obj.getMark() == WHITE) {
+					obj.setMark(GRAY);
+					toScan.add(obj);
+					controller.refreshView();      // Anzeige aktualisieren
+				}
+			} else {                // alle Basisobjekte abgearbeitet
+				gcTimer.stop();     // diesen Timer anhalten
+				markTimed();        // nächste Mark-Phase ausführen
+			}
+		});
+		gcTimer.start();            // starte erzeugten Timer
+	}
